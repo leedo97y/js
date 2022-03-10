6abe3f8659f4530b980c174fd3535e76c3aa62e4
leedo97y <dlehdus1433@gmail.com>
2022-03-11 02:56:00 +0900

    03/11 형변환 : 불리언 부분 추가

--- 0310_trans_type.js
+++ 0310_trans_type.js
@@ -23,3 +23,16 @@ console.log(Number(undefined)); // NaN
 console.log(parseInt("123.123")); // 123 -> 정수만
 console.log(parseFloat("123.123")); // 123.123 -> 실수까지 포함
 
+// Boolean 형변환 예제
+console.log(Boolean("")); //false
+console.log(Boolean("123")); // true
+console.log(Boolean("hello")); // true
+console.log(Boolean("0")); // true
+console.log(Boolean(0)); // false
+console.log(Boolean(123)); // true
+console.log(Boolean(NaN));  // false
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)); // false
+// 숫자, 문자열 : true
+// 숫자 0 : false 
+// null, undefined : false
