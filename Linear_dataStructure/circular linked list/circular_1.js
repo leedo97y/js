99b24b17b53c56f03441bcc0d4368d0cb3fcdafe
leedo97y <dlehdus1433@gmail.com>
2022-03-27 21:49:13 +0900

    원형 연결 리스트

---
+++ Linear_dataStructure/circular linked list/circular_1.js
@@ -0,0 +1,11 @@
+/*
+>> 원형 연결 리스트 
+각 노드가 데이터와 포인터를 가지며, 원형 상태로 연결되어 있는 방식으로 데이터를 저장하는 자료구조
+
+> 구현 메서드
+1) 노드 개수 / 비어있는지 확인 : CircularLinkedList.size(), CircularLinkedList.isEmpty()
+2) 노드 추가 : CircularLinkedList.append(), CircularLinkedList.insert()
+3) 노드 삭제 : CircularLinkedList.remove(), CircularLinkedList.removeAt()
+4) 데이터 위치 확인 / 노드 출력 : CircularLinkedList.indexOf(), CircularLinkedList.printNode()
+
+*/
