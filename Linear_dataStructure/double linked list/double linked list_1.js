9267722579c64d41cc69253b35645f0768b7ba03
leedo97y <dlehdus1433@gmail.com>
2022-03-27 21:49:25 +0900

    이중 연결 리스트

---
+++ Linear_dataStructure/double linked list/double linked list_1.js
@@ -0,0 +1,12 @@
+/*
+>> 이중 연결 리스트
+각 노드가 데이터와 포인터를 가지며, 두 줄로 연결되어 있는 방식으로 데이터를 저장하는 자료 구조
+
+> 구현 메서드
+1) 노드 개수 / 비어 있는지 확인 : DobleLinkedList.size(), DobleLinkedList.isEmpty()
+2) 순차 출력 / 역 출력 : DobleLinkedList.printNode(), DobleLinkedList.printNodeInverse()
+3) 노드 추가 : DobleLinkedList.append(), DobleLinkedList.insert()
+4) 노드 삭제 : DobleLinkedList.remove(), DobleLinkedList.removeAt()
+5) 데이터 위치 확인 : DobleLinkedList.indexOf()
+
+*/
