51af7d356563b666a3b4346dbbfbe8372a2b52d7
leedo97y <dlehdus1433@gmail.com>
2022-03-29 23:43:56 +0900

    stack

---
+++ Linear_dataStructure/stack/stack_1.js
@@ -0,0 +1,9 @@
+/* 
+>> 스택 (stack)
+- 나중에 넣은 데이터가 먼저나오는 LIFO 기반의 선형 자료구조
+
+> 구현메서드 
+데이터 전체 획득 / 비어있는지 확인 : Stack.getBuffer(), Stack.isEmpty()
+추가 / 삭제 / 마지막 데이터 조회 / 크기 확인 : Stack.push(), Stack.pop(), Stack.peak(), Stack.size()
+데이터 위치 / 존재 여부 확인 : Stack.indexOf(), Stack.includes()
+*/
