890eecb16df3fa51e81718d1dcccca34f0979356
leedo97y <dlehdus1433@gmail.com>
2022-04-25 21:10:00 +0900

    bubblesort

--- Algorithm/BubbleSort.js
+++ Algorithm/BubbleSort.js
@@ -9,6 +9,7 @@
     - 3. N 차례 반복 N - i
 */
 
+/* 거품정렬 (1) */
 let swap = function (arr, idx_1, idx_2) {
   let tmp = arr[idx_1];
   arr[idx_1] = arr[idx_2];
@@ -16,9 +17,9 @@ let swap = function (arr, idx_1, idx_2) {
 };
 
 let bubbleSort_1 = function (arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
+  for (let i = 0; i < arr.length - 1; i++) { // 총 수행 횟수
+    for (let j = 0; j < arr.length - 1; j++) { 
+      if (arr[j] > arr[j + 1]) { // 인접한 값끼리 비교
         swap(arr, j, j + 1);
       }
     }
@@ -27,7 +28,7 @@ let bubbleSort_1 = function (arr) {
 
 let bubbleSort_2 = function (arr) {
   for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
+    for (let j = 0; j < arr.length - i - 1; j++) { // - i 를 통해 이미 정렬된 부분은 다시 건드리지 않도록 처리
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
       }
@@ -38,14 +39,14 @@ let bubbleSort_2 = function (arr) {
 let bubbleSort_3 = function (arr) {
   let swapped;
   for (let i = 0; i < arr.length - 1; i++) {
-    swapped = false;
+    swapped = false; // for문 수행 전에 false 처리
     for (let j = 0; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
-        swapped = true;
+        swapped = true; // 한번만 swap 되어도 true 처리함
       }
     }
-    if (!swapped) break;
+    if (!swapped) break; // 이미 정렬된 상태에서는 break 걸림
   }
 };
 
@@ -61,3 +62,55 @@ console.log(array);
 array = [...init_array];
 bubbleSort_3(array);
 console.log(array);
+
+
+/* 거품정렬 (2) */
+let swap_1 = function (arr, idx_1, idx_2) {
+  let tmp = arr[idx_1];
+  arr[idx_1] = arr[idx_2];
+  arr[idx_2] = tmp;
+};
+
+let ascending = function (x, y) {
+  return x > y;
+};
+
+let descending = function (x, y) {
+  return x < y;
+};
+
+let bubbleSort = function (arr, compare) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (compare(arr[j], arr[j + 1])) {
+        swap_1(arr, j, j + 1);
+      }
+    }
+  }
+};
+
+/* test code */
+let init_array_1 = [6, 5, 1, 3, 2, 4];
+let array_1;
+
+let sorting = [bubbleSort];
+let order = [ascending, descending];
+for (let i = 0; i < sorting.length; i++) {
+  for (let j = 0; j < order.length; j++) {
+    console.log(sorting[i].name, order[j].name);
+
+    array_1 = [...init_array_1];
+    sorting[i](array_1, order[j]);
+    console.log(array_1);
+  }
+}
+
+/*
+< Output >
+
+bubbleSort ascending
+[ 1, 2, 3, 4, 5, 6 ]
+bubbleSort descending
+[ 6, 5, 4, 3, 2, 1 ] 
+
+*/
